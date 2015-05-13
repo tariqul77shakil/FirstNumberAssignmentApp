bb5b7df2bd2b9e1e769d66f4db851bb476b0e951
tariqul77shakil <shakil77cse@gmail.com>
2015-05-13 23:17:00 +0600

    Uploaded

---
+++ FirstNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Lower Limit");
+            int lowerLimit = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Upper Limit");
+            int UpperLImit = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nAll Integer between "+lowerLimit+" and "+UpperLImit+"\n");
+            for (int i = lowerLimit; i <= UpperLImit; i++)
+            {
+                Console.Write(i+" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
